124986ce15a69d82517e1ec65a364a8473bfc6e0
brihopki <brihopki@cisco.com>
2017-09-07 09:59:01 -0400

    adding files

---
+++ mycroft.py
@@ -0,0 +1,24 @@
+from errbot import BotPlugin, botcmd, arg_botcmd, webhook, re_botmcd
+import re
+
+class Mycroft(BotPlugin):
+    """
+    ask questions about mycroft and interact with a mycroft instance
+    """
+
+    @webhook
+    def example_webhook(self, incoming_request):
+        """A webhook which simply returns 'Example'"""
+        return "Example"
+
+    # Passing split_args_with=None will cause arguments to be split on any kind
+    # of whitespace, just like Python's split() does
+    @re_botcmd(pattern=r"doc.*mycroft|mycroft.*doc.*", prefixed=False, flags=re.IGNORECASE)
+    def mycroft_docs(self, message, match):
+        """A command which gives you the location to the mycroft documentation"""
+        return "The mycroft documentation can be found at https://docs.mycroft.ai"
+
+    @re_botcmd(pattern=r"install.*mycroft|mycroft.*install.*", prefixed=False, flags=re.IGNORECASE)
+    def install_mycroft(self, message, match):
+        """A command which gives you the location to the mycroft documentation"""
+        return "The information for installing mycroft can be found here, https://docs.mycroft.ai/installing.and.running"
