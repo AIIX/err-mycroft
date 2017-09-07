56b62e5e1604d96a6bfe9c67af26a0ba5c68dc32
brihopki <brihopki@cisco.com>
2017-09-07 11:06:12 -0400

    fixing pep

--- mycroft.py
+++ mycroft.py
@@ -28,10 +28,9 @@ class Mycroft(BotPlugin):
         return "Mycroft Core is the primary module that makes up the \
         Mycroft Artificial Intelligence platform. Mycroft makes use of \
         the Adapt Intent Parser, Speech-to-Text software, and Text-to-Speech.\
-        The idea behind the platform is to be able to voice enable any device \
-        and turn it into a smart personal assistant, able to perform a variety \
-        of tasks. Mycroft is often used to refer to the hardware product produced \
-        by Mycroft AI, Inc. - so to avoid confusion, the software stack is often \
+        The idea behind the platform is to be able to voice enable any device\
+        and turn it into a smart personal assistant, able to perform a variety\
+        of tasks. Mycroft is often used to refer to the hardware product produced\
+        by Mycroft AI, Inc. - so to avoid confusion, the software stack is often\
         referred to as \"Mycroft Core\", more info can be found here, \ " \
         "https://docs.mycroft.ai/"
-
