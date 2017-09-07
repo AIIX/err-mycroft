850c2a0e3e34dca690a7cdd61cb7c20a5dde54a6
brihopki <brihopki@cisco.com>
2017-09-07 12:10:20 -0400

    fixing spaces

--- mycroft.py
+++ mycroft.py
@@ -12,7 +12,7 @@ class Mycroft(BotPlugin):
     def mycroft_docs(self, message, match):
         """A command which gives you the location to the mycroft docs"""
 
-        output = "The mycroft documentation can be found at" \
+        output = "The mycroft documentation can be found at " \
                  "https://docs.mycroft.ai"
 
         return output
@@ -22,7 +22,7 @@ class Mycroft(BotPlugin):
     def install_mycroft(self, message, match):
 
         """A command which gives you the location to the mycroft install"""
-        output = "The information for installing mycroft can be found here," \
+        output = "The information for installing mycroft can be found here, " \
                  "https://docs.mycroft.ai/installing.and.running"
 
         return output
@@ -32,14 +32,14 @@ class Mycroft(BotPlugin):
     def what_mycroft(self, message, match):
 
         """A command which gives you information about mycroft"""
-        output = "Mycroft Core is the primary module that makes up the" \
-                 "Mycroft Artificial Intelligence platform.  Mycroft makes use of" \
-                 "the Adapt Intent Parser, Speech-to-Text software, and Text-to-Speech." \
-                 "The idea behind the platform is to be able to voice enable any device" \
-                 "and turn it into a smart personal assistant, able to perform a variety" \
-                 "of tasks. Mycroft is often used to refer to the hardware product" \
-                 "produced by Mycroft AI, Inc. - so to avoid confusion, the software" \
-                 "stack is often referred to as \"Mycroft Core\", more info can be" \
+        output = "Mycroft Core is the primary module that makes up the " \
+                 "Mycroft Artificial Intelligence platform.  Mycroft makes use of " \
+                 "the Adapt Intent Parser, Speech-to-Text software, and Text-to-Speech. " \
+                 "The idea behind the platform is to be able to voice enable any device " \
+                 "and turn it into a smart personal assistant, able to perform a variety " \
+                 "of tasks. Mycroft is often used to refer to the hardware product " \
+                 "produced by Mycroft AI, Inc. - so to avoid confusion, the software " \
+                 "stack is often referred to as \"Mycroft Core\", more info can be " \
                  "found here, \"https://docs.mycroft.ai/"
 
         return output
