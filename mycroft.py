c178c7164f1d49517d0fee4946374d6045f3bc46
Aditya Mehra <Aix.m@outlook.com>
2017-09-12 23:05:48 +0530

    Added desktop section

--- mycroft.py
+++ mycroft.py
@@ -55,6 +55,33 @@ class Mycroft(BotPlugin):
                  "here, https://github.com/MycroftAI/enclosure-picroft/wiki" \
                  "/Getting-Started-Guide."
         return output
+    
+    @botcmd
+    def install_plasmoid(self, mess, args):
+        """A command that gives information about installing the KDE Plasmoid"""
+        output = "The install guide for the KDE plasmoid project can be found " \
+                 "here, https://cgit.kde.org/plasma-mycroft.git/tree/Readme.md" \
+                 "If you are running a Debian/Ubuntu or Fedora based distribution" \
+                 "You can find the installation scripts here, https://github.com/MycroftAI/installers"
+        return output
+    
+    @botcmd
+    def install_qtapp(self, mess, args):
+        """A command that gives information about installing the QT Application"""
+        output = "The appimage for the qt standalone application project can be found " \
+                 "here, https://github.com/AIIX/Mycroft-Ai-QtApplication/releases"
+        return output
+    
+    @botcmd
+    def install_qtapp(self, mess, args):
+        """A command that gives troublehooting information about the KDE Plasmoid"""
+        output = "Steps to troubleshoot your plasmoid install" \
+                 "* Check if mycroft-core is installed correctly" \
+                 "* Open plasmoid settings and check your mycroft path" \
+                 "* Run plasmashell in debug mode and check for error messages" \
+                 "* Submit your issue/bug report on the #desktop channel, or" \
+                 "* Create a bug report here, https://bugs.kde.org/describecomponents.cgi?product=plasma-mycroft"
+        return output
 
     @botcmd
     def picroft_docs(self, mess, args):
@@ -82,6 +109,14 @@ class Mycroft(BotPlugin):
         """A command that links you the skills repo"""
         output = "The current list of mycroft skills can be found here, " \
                  "https://github.com/MycroftAI/mycroft-skills"
+        return output
+    
+    @botcmd
+    def desktop_clients(self, mess, args):
+        """A command that list currently available desktop clients"""
+        output = "The current list of mycroft desktop clients, " \
+                 "KDE Plasma Desktop: https://cgit.kde.org/plasma-mycroft.git/tree/Readme.md" \
+                 "Other Desktop Environments: https://github.com/AIIX/Mycroft-AI-QtApplication/releases"
         return output
 
     @botcmd
